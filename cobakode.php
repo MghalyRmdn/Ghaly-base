261cca0b1ce7457739066841f150b75068627c20
Mochammad Ghalie Rmdn <44633669+MghalyRmdn@users.noreply.github.com>
2019-05-18 08:16:04 +0700

    cobakode.php

---
+++ cobakode.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php pertama kali</title>
+<h1 align="center">Selamat Berpuasa</h1>
+</head>
+
+<body>
+<?php
+$nama = "Pesantren Koding";
+$nama2 = Yuk belajar';
+echo "Selamat datang di $nama<br>";
+//tipe data angka/integer
+//1.integer 2.float
+$angka = 1000;
+$angkas = 100;
+
+echo $angka + $angkas ;
+
+?>
+</body>
+</html>
